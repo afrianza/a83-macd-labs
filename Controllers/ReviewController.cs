1a88c4a22412f5bc2d8ab33448546508695e886b
afrianza <aldoafrianda@gmail.com>
2020-07-22 17:46:03 +0700

    commit

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -10,7 +10,7 @@ namespace BooksCatalogue.Controllers
 {
     public class ReviewController : Controller
     {
-        private string apiEndpoint = "https://bookscatalogueapi-dicoding.azurewebsites.net/api/";
+        private string apiEndpoint = "https://katalog-api.azurewebsites.net/api/review/";
 
         public ReviewController() {
         }
