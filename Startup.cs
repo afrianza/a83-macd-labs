9ec282c0327c456b9ef22091ed5dc80810d022bb
afrianza <aldoafrianda@gmail.com>
2020-07-23 09:05:34 +0700

    Revert "commit"
    This reverts commit b615aee7fb397f58b2f870959edaec4bf2990b35.

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using BooksCatalogueAPI.Models;
 
 namespace BooksCatalogueAPI
 {
@@ -30,7 +29,6 @@ namespace BooksCatalogueAPI
         {
             services.AddControllers();
             services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            services.Configure<AzureStorageConfig>(Configuration.GetSection("AzureStorageConfig"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
