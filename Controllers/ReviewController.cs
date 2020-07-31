5fc9bbf7b514a0be5a9e651b0d4e3f7b7ee75760
afrianza <aldoafrianda@gmail.com>
2020-08-01 06:26:22 +0700

    Revert "commit"
    This reverts commit 119d674de9ff5936ed8d4f2152b32ad811e84731.

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -18,7 +18,6 @@ namespace BooksCatalogue.Controllers
             _client = new HttpClient(clientHandler);
         }
 
-        // Acuan Back to Details
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
