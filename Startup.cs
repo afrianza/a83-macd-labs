59c81c5a35b5b3b831745003596ad8fa9a3ab0cf
afrianza <aldoafrianda@gmail.com>
2020-08-05 12:10:12 +0700

    commit

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace BooksCatalogue
         {
             services.AddControllersWithViews();
             services.Configure<AzureSearchService>(Configuration.GetSection("AzureSearchService"));
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +56,8 @@ namespace BooksCatalogue
                     name: "default",
                     pattern: "{controller=Books}/{action=Index}/{id?}");
             });
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
         }
         
     }
